e61f86c9cafab3cc5d660afe7a78c538dc16e63c
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-02-26 16:32:36 +0100

    Add basic project setup, basic styles to home page and bootswatch styles, font awesome icon.

--- djpayment/donation/views.py
+++ djpayment/donation/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def my_donation(request):
+
+    return render(request, template_name="donation/my-donation.html")
+
+
+def payment_success(request):
+
+    return render(request, template_name="donation/payment-success.html")
+
+
+def payment_failed(request):
+
+    return render(request, template_name="donation/payment-failed.html")
