ad50337abb199763ea3b267941cec03130978a06
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-03-05 19:57:27 +0100

    Add Donation and Transaction models to models.py in donation app.

--- djpayment/donation/models.py
+++ djpayment/donation/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Donation(models.Model):
+
+    title = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} ({self.amount})"
+
+
+class Transaction(models.Model):
+
+    invoice = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=4, decimal_places=2)
+    paid = models.BooleanField(default=False)
+
